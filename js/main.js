3ce37a4ad0e32c970453c7c14918ff44b2330c20
AdonayMorales29 <zadonay111@gmail.com>
2019-08-07 11:54:09 -0600

    Proyecto

---
+++ js/main.js
@@ -0,0 +1,55 @@
+//variables globales
+let canales=[
+   { 
+       id:1,
+       nombre:"Hch",
+       descripcion:"Canal de telenoticias"
+    },
+    { 
+        id:1,
+        nombre:"Anime",
+        descripcion:"Canal de Anime"
+     },
+    ];
+let videos=[
+    {
+        titulo:"Marcha de las antorchas",
+        url:"img/1.jpg",
+        canal:"Fuera JOH",
+        Visualizaciones:"20k",
+        duracion:"22:23",
+        tiempo:"19 hours ago"
+    },
+    {
+        titulo:"Marcha de las antorchas",
+        url:"img/1.jpg",
+        canal:"Fuera JOH",
+        Visualizaciones:"20k",
+        duracion:"22:23",
+        tiempo:"19 hours ago"
+    },
+    {
+        titulo:"Marcha de las antorchas",
+        url:"img/1.jpg",
+        canal:"Fuera JOH",
+        Visualizaciones:"20k",
+        duracion:"22:23",
+        tiempo:"19 hours ago"
+    },
+    {
+        titulo:"Marcha de las antorchas",
+        url:"img/1.jpg",
+        canal:"Fuera JOH",
+        Visualizaciones:"20k",
+        duracion:"22:23",
+        tiempo:"19 hours ago"
+    },
+    {
+        titulo:"Marcha de las antorchas",
+        url:"img/1.jpg",
+        canal:"Fuera JOH",
+        Visualizaciones:"20k",
+        duracion:"22:23",
+        tiempo:"19 hours ago"
+    }
+]
