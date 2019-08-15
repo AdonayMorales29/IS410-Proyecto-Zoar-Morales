39f5f572042ba5dd9aa0b5fdf34bf0d793ebe3bc
AdonayMorales29 <zadonay111@gmail.com>
2019-08-15 17:34:01 -0600

    editando

--- js/mainProducto.js
+++ js/mainProducto.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     // clase al primer enlace
- $('.categorias .categoria_item[categoria="all"]').addClass('.ct_item-active');
+    $('.categorias .categoria_item[categoria="all"]').addClass('.ct_item-active');
+    $('.categotias_item').click(function(){
+
+    });
      
 });
