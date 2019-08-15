d3e234711802516dbe8f9b69a9e915c9923185d8
AdonayMorales29 <zadonay111@gmail.com>
2019-08-14 22:36:40 -0600

    actualizacion

--- js/mainProducto.js
+++ js/mainProducto.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
     // clase al primer enlace
- $('.categorias .categoria_item[categoria="all"]').addClass('ct_item-active');
-
- $('categoria_item').click(function(){
-     $('.categoria_item').removeClass('ct_item-active');
-     $(this).addClass('ct_item-active');
-    
-     console.log('categoria_item');
+ $('.categorias .categoria_item[categoria="all"]').addClass('.ct_item-active');
      
- })
 });
