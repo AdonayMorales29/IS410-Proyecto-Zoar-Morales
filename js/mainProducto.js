488ec97777b0ae026095c4f9c7f53665a37aa864
AdonayMorales29 <zadonay111@gmail.com>
2019-08-17 17:17:06 -0600

    editando productos

--- js/mainProducto.js
+++ js/mainProducto.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
     // clase al primer enlace
-    $('.categorias .categoria_item[categoria="all"]').addClass('.ct_item-active');
-    $('.categotias_item').click(function(){
+    $('.categorias .categoria_item[categoria="all"]').addClass('ct_item-active');
+    $('.categoria_item').click(function(){
+        var seleccionarProducto=$(this).attr('categoria');
 
+        //Activar color al enlace y filtrando productos
+        $('.categoria_item').removeClass('ct_item-active');
+        $(this).addClass('ct_item-active');
+        //ocultar Productos
+
+        $('.productos-Item').css('transform', 'scale(0)');
+        function hideProductos(){
+            $('.productos-Item').hide();
+        }setTimeout(hideProductos,400);
+
+
+        //Muestra Productos
+
+        function showProductos(){
+            $('.productos-Item[categoria="'+seleccionarProducto+'"').show();
+            $('.productos-Item[categoria="'+seleccionarProducto+'"').css('transform', 'scale(1)');
+        }setTimeout(showProductos,800);
+           
+
+        
     });
+    //Muestra todos los productos
+   $('.categoria_item[categoria="All"]').click(function(){
+       function showAll(){
+        $('.productos-Item').show();
+        $('.productos-Item').css('transform', 'scale(1)');
+       }setTimeout(showAll,400);
+           
+  
+
+   });
      
 });
