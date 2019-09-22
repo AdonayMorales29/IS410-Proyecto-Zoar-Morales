f0dc9e2aad10f0e757a4f0abb6089a423a5a7473
AdonayMorales29 <zadonay111@gmail.com>
2019-09-21 23:34:39 -0600

    ..
    ...

--- js/controladorInicioSesion.js
+++ js/controladorInicioSesion.js
@@ -1,7 +1,53 @@
 $("#login").click(function(event){
 
 
-console.log("login")
+//console.log("login");
+//valores de los input
+
+var correo=$("#correo").val();
+var contraseña=$("#contraseña").val();
+
+//console.log(correo + "" + contraseña);
+
+var parametro={pcorreo:correo, pcontraseña:contraseña};
+//console.log(parametro);
+
+$.ajax({
+    url:'ajax/controladorInicioSesion.php?accion=login',
+    method:'POST',
+    dataType:'json',
+    data:parametro,
+
+    success:function(res){
+        console.log(res);
+
+        if (res.res==1){
+            console.log("valido");
+            location.href="http://www.cristalab.com/foros";
+
+			}else{
+				console.log("Invalido");
+
+			}
+
+        }
+
+    }
 
 
 })
+function detalles(id) {
+	console.log(id);
+
+}
+function login() {
+
+	console.log("Funcion login");
+
+	//obtener los valores de un input
+	var correo = $("#correo").val();
+	var contrasenia = $("#contrasenia").val();
+
+	console.log(contrasenia + " " + correo);
+}
+
